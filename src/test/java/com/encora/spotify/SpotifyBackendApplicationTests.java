2f53f5eb45021c2eabb4b64be77e1d23eeacb43c
OmarBustamante <omar.bustamante@encora.com>
2025-05-05 15:28:46 -0700

    Initial commit

---
+++ src/test/java/com/encora/spotify/SpotifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
